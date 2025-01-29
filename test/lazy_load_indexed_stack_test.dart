4d475066d597714e616fde785d957fefcd1164a5
okaryo <okaryotr@gmail.com>
2025-01-29 15:34:13 +0900

    test: split existing tests to default and #preloadIndexes # ==== Emojis ==== # ✨ :sparkles: feature additions/upgrades # 🐛 :bug: bug fixes # 🩹 :adhesive_bandage: fixes minor issues # ♻️ :recycle: refactoring # 🎨 :art: fixes Lint errors and code styles # 🔥 :fire: remove unneeded or obsolete features # ⚡️ :zap: performance improvements # ⬆️ :arrow_up: update dependent packages etc. # 🔒 :lock: security improvements # ✅ :white_check_mark: test additions and modifications # 💚 :green_heart: CI fixes and improvements # 📝 :memo: documentation additions/fixes. # 🚧 :construction: work in progress

--- test/lazy_load_indexed_stack_test.dart
+++ test/lazy_load_indexed_stack_test.dart
@@ -3,32 +3,57 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lazy_load_indexed_stack/lazy_load_indexed_stack.dart';
 
 void main() {
-  testWidgets('LazyLoadIndexedStack', (final WidgetTester tester) async {
-    const key = Key('key');
-    final lazyLoadIndexedStack = LazyLoadIndexedStack(
-      key: key,
-      index: 0,
-      preloadIndexes: const [1, 3],
-      children: [
-        _buildWidget(1),
-        _buildWidget(2),
-        _buildWidget(3),
-        _buildWidget(4),
-        _buildWidget(5),
-      ],
-    );
+  group('LazyLoadIndexedStack', () {
+    group('default behavior', () {
+      testWidgets('only the selected index is loaded', (tester) async {
+        const key = Key('default_test');
+        final lazyLoadIndexedStack = LazyLoadIndexedStack(
+          key: key,
+          index: 0,
+          children: [
+            _buildWidget(1),
+            _buildWidget(2),
+            _buildWidget(3),
+            _buildWidget(4),
+            _buildWidget(5),
+          ],
+        );
 
-    await tester.pumpWidget(MaterialApp(home: lazyLoadIndexedStack));
+        await tester.pumpWidget(MaterialApp(home: lazyLoadIndexedStack));
 
-    final StatefulElement element = tester.element(find.byKey(key));
-    final elementState = element.state as State<LazyLoadIndexedStack>;
-    expect(elementState.widget, equals(lazyLoadIndexedStack));
+        expect(find.text('page1', skipOffstage: false), findsOneWidget);
+        expect(find.text('page2', skipOffstage: false), findsNothing);
+        expect(find.text('page3', skipOffstage: false), findsNothing);
+        expect(find.text('page4', skipOffstage: false), findsNothing);
+        expect(find.text('page5', skipOffstage: false), findsNothing);
+      });
+    });
 
-    expect(find.text('page1', skipOffstage: false), findsOneWidget);
-    expect(find.text('page2', skipOffstage: false), findsOneWidget);
-    expect(find.text('page3', skipOffstage: false), findsNothing);
-    expect(find.text('page4', skipOffstage: false), findsOneWidget);
-    expect(find.text('page5', skipOffstage: false), findsNothing);
+    group('#preloadIndexes', () {
+      testWidgets('Only indexes in preloadIndexes should be preloaded', (tester) async {
+        const key = Key('preload_test');
+        final lazyLoadIndexedStack = LazyLoadIndexedStack(
+          key: key,
+          index: 0,
+          preloadIndexes: const [1, 3],
+          children: [
+            _buildWidget(1),
+            _buildWidget(2),
+            _buildWidget(3),
+            _buildWidget(4),
+            _buildWidget(5),
+          ],
+        );
+
+        await tester.pumpWidget(MaterialApp(home: lazyLoadIndexedStack));
+
+        expect(find.text('page1', skipOffstage: false), findsOneWidget);
+        expect(find.text('page2', skipOffstage: false), findsOneWidget);
+        expect(find.text('page3', skipOffstage: false), findsNothing);
+        expect(find.text('page4', skipOffstage: false), findsOneWidget);
+        expect(find.text('page5', skipOffstage: false), findsNothing);
+      });
+    });
   });
 }
 
